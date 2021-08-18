06b4f6dee0d671b43e2d69033e2a24067e92b6c8
prajotsurey <prajotsurey@gmail.com>
2021-08-18 17:36:32 +0530

    Fixed "Property 'userId' does not exist on type 'Session & Partial <SessionData>'" error

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,12 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { Request, Response } from "express";
+import { Session, SessionData } from 'express-session';
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request;
+  req: Request & {
+    session: Session & Partial<SessionData> & { userId?: number };
+  };
   res: Response;
 };
 
