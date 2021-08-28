9503670b90c4ab1668c827f0c7fa48590195ed9e
prajotsurey <prajotsurey@gmail.com>
2021-08-28 19:14:31 +0530

    updated delete post resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -174,8 +174,12 @@ export class PostResolver {
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg('id') id: number) : Promise<Boolean> {
-    await Post.delete(id)
+  @UseMiddleware(isAuth)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { req }: MyContext
+    ) : Promise<Boolean> {
+    await Post.delete({ id, creatorId: req.session.userId});
     return true;
   }
 
